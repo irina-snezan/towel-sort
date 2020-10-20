b5941228693b650d201b2fc2b31b8a1a9c70f71e
Irina Shabuldajeva <70856550+irina-snezan@users.noreply.github.com>
2020-10-20 21:08:58 +0300

    feat: add cod

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (typeof matrix === 'undefined')
+		return [];
+	var result = [];
+	matrix.forEach(function (arr, i) {result = (i % 2 === 1) ? result.concat(arr.reverse()) : result = result.concat(arr);});
+	return result;
 }
+ 
